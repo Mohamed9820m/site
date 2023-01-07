6f27302fa5f4f0894847d1af641d717113f2a1bb
Mohamed9820m <75361522+Mohamed9820m@users.noreply.github.com>
2023-01-07 03:34:35 +0100

    first weeb

---
+++ app/config.php
@@ -0,0 +1,8 @@
+<?php
+//Config Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'devil_dbb');
+define('DB_PASS', 'devil_dbb');
+define('DB_NAME', 'devil_dbb');
+define('TIMEZONE', 'Asia/Kolkata');
+define('ENCRYPTION_KEY', 'c5fbb0694cb9c7e6dda5926478149672');
